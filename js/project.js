676b5d497022a5e0e033e316656f9d2cae0acd7a
Merlin Mason <info@merlinmason.co.uk>
2013-10-21 22:33:10 +0100

    improve file structure and add window load event

--- js/project.js
+++ js/project.js
@@ -1,3 +1,24 @@
-$(function() {
-	console.log("jQuery is running!");
-});
+var wpstarter = {
+
+	init: function () {
+		this.bindUIActions();
+	},
+
+	bindUIActions: function () {
+		$(".btn").on("click", function(e) { wpstarter.sayHello(e); });
+	},
+
+	windowLoaded: function () {
+		console.log("Loaded")
+	},
+
+	sayHello: function (e) {
+		console.log("Hello World");
+	}
+
+};
+
+// DOM Ready
+$(function () { wpstarter.init(); });
+// Images Loaded
+$(window).load(function() { wpstarter.windowLoaded(); });
