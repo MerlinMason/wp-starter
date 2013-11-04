fc82527f96bca3ed20b4117123c3fb3665ccfea7
Merlin Mason <info@merlinmason.co.uk>
2013-11-04 16:02:36 +0000

    bower changed directory name in V1

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // call minifed jQuery from bower
 function wpstarter_jquery_enqueue() {
 	wp_deregister_script('jquery');
-	wp_register_script('jquery', "/wp-content/themes/wp-starter/bower_components/jquery/jquery.min.js", false, null);
+	wp_register_script('jquery', "/wp-content/themes/wp-starter/components/jquery/jquery.min.js", false, null);
 	wp_enqueue_script('jquery');
 }
 if (!is_admin()) add_action("wpstarter_enqueue_scripts", "wpstarter_jquery_enqueue", 11);
