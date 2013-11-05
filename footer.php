53ac84d7be40fe3a906d76c3f3b7edd1ba9ed705
Merlin Mason <info@merlinmason.co.uk>
2013-11-05 19:04:58 +0000

    pull in different files for dev and production

--- footer.php
+++ footer.php
@@ -4,7 +4,28 @@
 
 </div>
 
-<?php wp_footer(); ?>
+<?php
+
+	wp_footer();
+
+	$production = false;
+
+	// if we're in production mode, include minified concatenated script
+	// if not then pull them in unminified individually for easier debugging
+	if (production) {
+
+		echo '<script src ="' . bloginfo("template_url") . '/js/production.js"></script>';
+
+	} else {
+
+		echo '<script src ="' . bloginfo("template_url") . '/components/jquery/jquery.js"></script>';
+		echo '<script src ="' . bloginfo("template_url") . '/components/modernizr/modernizr.js"></script>';
+		echo '<script src ="' . bloginfo("template_url") . '/js/project.js"></script>';
+
+	}
+
+?>
+
 
 </body>
 </html>
