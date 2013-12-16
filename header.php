e97c6526b1e4a025fc60d0de99a9e37d077a3e5a
Merlin Mason <info@merlinmason.co.uk>
2013-12-16 17:31:35 +0000

    add minimal-ui meta tag for iOS7

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<!-- Meta tags -->
 	<meta charset="utf-8">
 
-	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="HandheldFriendly" content="true">
 
